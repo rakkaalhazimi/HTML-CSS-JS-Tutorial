7f5fa7be14574e73c0a55acff63e875cbb75c506
Rakka Alhazimi <rakkakeren@gmail.com>
2024-04-06 21:10:09 +0700

    change: insert wave logic inside function

--- Basic Javascript/web-audio-api/visualize-waveforms/index.js
+++ Basic Javascript/web-audio-api/visualize-waveforms/index.js
@@ -1,18 +1,68 @@
 let audioContext = new AudioContext();
 let audioFile = document.getElementById("audio-file");
-let canvasWave = document.getElementById("waveform-canvas");
-let canvasWaveContext = canvasWave.getContext("2d");
-let canvasBlock = document.getElementById("block-canvas");
-let canvasBlockContext = canvasBlock.getContext("2d");
-let CANVAS_WIDTH = canvasWave.clientWidth;
-let CANVAS_HEIGHT = canvasWave.clientHeight;
 
 
-canvasWaveContext.fillStyle = "rgb(240 240 240)";
-canvasWaveContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-canvasBlockContext.fillStyle = "rgb(240 240 240)";
-canvasBlockContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-// canvasContext.reset();
+
+
+function showWavePlot(waveform) {
+  
+  let canvas = document.getElementById("waveform-canvas");
+  let context = canvas.getContext("2d");
+  let CANVAS_WIDTH = canvas.clientWidth;
+  let CANVAS_HEIGHT = canvas.clientHeight;
+  
+  context.fillStyle = "rgb(240 240 240)";
+  context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  context.fillStyle = "green";
+  
+  let waveWidth = CANVAS_WIDTH / waveform.length;
+  for (let i = 0; i < waveform.length; i++) {
+    
+    let sample = waveform[i];
+    let x = i * waveWidth;
+    let y = Math.floor(CANVAS_HEIGHT / 2);
+    let width = 1;
+    let height = (sample * Math.floor(CANVAS_HEIGHT / 2)) + 1;
+    
+    context.fillRect(x, y, width, height);
+  }
+}
+
+
+function showWaveBarPlot(waveform) {
+  
+  let canvas = document.getElementById("block-canvas");
+  let context = canvas.getContext("2d");
+  let CANVAS_WIDTH = canvas.clientWidth;
+  let CANVAS_HEIGHT = canvas.clientHeight;
+
+  context.fillStyle = "rgb(240 240 240)";
+  context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  context.strokeStyle = "green";
+  context.fillStyle = "green";
+  
+  let blockCount = 100;
+  let blockWidth = CANVAS_WIDTH / blockCount;
+  let waveStep = Math.floor(waveform.length / blockCount);
+  
+  for (let i = 0; i < blockCount; i++) {
+    
+    let samples = waveform.slice(i * waveStep, (i + 1) * waveStep);
+    let sample = Math.max(...samples);
+
+    let x = i * blockWidth;
+    let y = Math.floor(CANVAS_HEIGHT / 2);
+    let width = blockWidth;
+    let height = Math.abs(sample * Math.floor(CANVAS_HEIGHT / 2)) + 4;
+    
+    context.beginPath();
+    context.roundRect(x, y + (-height), width * 0.5, height * 2, [40]);
+    context.fill();
+    
+    // canvasBlockContext.fillRect(x, y + (-height), width * 0.5, height * 2);
+  }
+}
+
 
 async function uploadAudio(event) {
   console.log("Upload audio");
@@ -21,57 +71,8 @@ async function uploadAudio(event) {
   let audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
   let audioWaveform = audioBuffer.getChannelData(0);
   
-  console.log("Max value: ", Math.max(...audioWaveform));
-  console.log("Min value: ", Math.min(...audioWaveform));
-  
-  // Wave Style
-  let waveWidth = CANVAS_WIDTH / audioWaveform.length;
-  for (let i = 0; i < audioWaveform.length; i++) {
-    
-    let sample = audioWaveform[i];
-    let x = i * waveWidth;
-    let y = Math.floor(CANVAS_HEIGHT / 2);
-    let width = 1;
-    let height = sample * Math.floor(CANVAS_HEIGHT / 2);
-
-    if (sample > 0) {
-      height = Math.max(height, 0.1);
-    } else {
-      height = Math.min(height, -0.1);
-    }
-    
-    canvasWaveContext.fillStyle = "green";
-    canvasWaveContext.fillRect(x, y, width, height);
-  }
-  
-  // Block Style
-  let blockCount = 100;
-  let blockWidth = CANVAS_WIDTH / blockCount;
-  let waveStep = Math.floor(audioWaveform.length / blockCount);
-  
-  for (let i = 0; i < blockCount; i++) {
-    
-    let samples = audioWaveform.slice(i * waveStep, (i + 1) * waveStep);
-    let sample = Math.max(...samples);
-    console.log(sample);
-
-    let x = i * blockWidth;
-    let y = Math.floor(CANVAS_HEIGHT / 2);
-    let width = blockWidth;
-    let height = Math.abs(sample * Math.floor(CANVAS_HEIGHT / 2));
-    
-    height = Math.max(height, 5);
-    height = Math.min(height, CANVAS_HEIGHT) * 0.7;
-    
-    canvasBlockContext.strokeStyle = "green";
-    canvasBlockContext.fillStyle = "green";
-    canvasBlockContext.beginPath();
-    canvasBlockContext.roundRect(x, y + (-height), width * 0.5, height * 2, [40]);
-    canvasBlockContext.fill();
-    
-    // canvasBlockContext.fillRect(x, y + (-height), width * 0.5, height * 2);
-  }
-  
+  showWavePlot(audioWaveform);
+  showWaveBarPlot(audioWaveform);
 }
 
 audioFile.addEventListener("change", uploadAudio);
